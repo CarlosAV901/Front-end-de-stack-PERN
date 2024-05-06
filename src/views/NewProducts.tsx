cdf2df7a35412586db70d0bc5422f3d2ca9a7c13
Charly <aguivega123@hotmail.com>
2024-05-05 23:09:47 -0600

    Se creo el router

---
+++ src/views/NewProducts.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function NewProduct() {
+    return (
+        <>
+            <div className="flex justify-between">
+                <h2 className="text-4xl font-black text-slate-500">Registrar Producto</h2>
+                <Link to='/'
+                    className="rounded-lg bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+                >
+                    Volver a productos
+                </Link>
+            </div>
+        </>
+    )
+}
